db62383624515788c5dddcc10aa2068780204bdc
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-14 13:37:38 +0300

    Initializer project and express configuration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+
+app.listen(3000, () => {console.log('Server is listening on port 3000')});
+app.use(routes)
